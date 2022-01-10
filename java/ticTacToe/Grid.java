a8526dd29db2212fc1e82667909bbe7d65e0aa49
jonah1441 <62459200+jonah1441@users.noreply.github.com>
2022-01-10 08:50:40 +0100

    Update Grid.java

--- java/ticTacToe/Grid.java
+++ java/ticTacToe/Grid.java
@@ -2,6 +2,10 @@ package com.company;
 
 import java.util.Scanner;
 
+
+/**
+ * grid defines the table
+ */
 public class Grid {
     Scanner scanner = new Scanner(System.in);
 
@@ -20,6 +24,9 @@ public class Grid {
 
     }
 
+    /**
+     * prints the tic tac toe board
+     */
     public void printBoard() {
         System.out.println(board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
         System.out.println("--" + "+" + "---" + "+" + "--");
@@ -28,73 +35,74 @@ public class Grid {
         System.out.println(board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
 
     }
-
-    public void play(int turns) {
+    /**
+     *defines the plays a player can play
+     */
+    public void playPlayer(int turns) {
 
         String play = "";
-        System.out.println("It's player " + player(turns) + "'s" + " turn.");
-        System.out.println("Where would you like to play?");
+        System.out.println("It's player " + player(turns) + "'s" + " turn."+ " Where you going to play?");
         play = scanner.nextLine();
         switch (play) {// if this
             case "1":
                 if (board[0][0].equals("X") || board[0][0].equals("O")) {
-                    play(turns);
+                    playPlayer(turns);
                 } else {
                     board[0][0] = player(turns);
                 }
                 break;
             case "2":
                 if (board[0][1].equals("X") || board[0][1].equals("O")) {
-                    play(turns);
+                    playPlayer(turns);
                 } else {
                     board[0][1] = player(turns);
                 }
                 break;
             case "3":
                 if (board[0][2].equals("X") || board[0][2].equals("O")) {
-                    play(turns);
+                    playPlayer(turns);
                 } else {
                     board[0][2] = player(turns);
                 }
                 break;
             case "4":
                 if (board[1][0].equals("X") || board[1][0].equals("O")) {
-                    play(turns);
+                    playPlayer(turns);
                 } else {
                     board[1][0] = player(turns);
                 }
                 break;
             case "5":
                 if (board[1][1].equals("X") || board[1][1].equals("O")) {
-                    play(turns);
+                    playPlayer(turns);
                 } else {
                     board[1][1] = player(turns);
                 }
                 break;
             case "6":
                 if (board[1][2].equals("X") || board[1][2].equals("O")) {
-                    play(turns);
+                    playPlayer(turns);
                 } else {
                     board[1][2] = player(turns);
                 }
                 break;
             case "7":
                 if (board[2][0].equals("X") || board[2][0].equals("O")) {
-                    play(turns);
+                    playPlayer(turns);
                 } else {
                     board[2][0] = player(turns);
                 }
                 break;
             case "8":
                 if (board[2][1].equals("X") || board[2][1].equals("O")) {
-                    play(turns);
+                    playPlayer(turns);
                 } else {
                     board[2][1] = player(turns);
                 }
                 break;
             case "9":
                 if (board[2][2].equals("X") || board[2][2].equals("O")) {
-                    play(turns);
+                    playPlayer(turns);
                 } else {
                     board[2][2] = player(turns);
                 }
@@ -104,6 +112,9 @@ public class Grid {
 
     }
 
+    /**
+     * returns the  players turn based on turns
+     */
     public String player(int turns) {
 
         if ((turns - 1) % 2 == 0) {
@@ -112,8 +123,10 @@ public class Grid {
             return "X";
         }
     }
-
-    public void winCheak() {
+    /**
+     *Checks if someone has won
+     */
+    public boolean winCheck(int turns) {
         String row = "";
         for (int i = 0; i < 9; i++) {
 
@@ -145,16 +158,24 @@ public class Grid {
 
 
             }
+
+            if (turns == 8){
+                System.out.println("its a draw");
+                return false;
+
+            }
             if (row.equals("XXX")) {
                 System.out.println("player X has won");
-                System.exit(0);
+                return false;
             }
             if (row.equals("OOO")) {
                 System.out.println("player O has won");
-                System.exit(0);
+                return false;
             }
 
+
         }
+        return true;
 
     }
 
